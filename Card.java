45ec094b4b34ece72f108f0e70fcc66199540294
Timothy <tjl219@exeter.ac.uk>
2023-11-12 23:31:25 +0000

    Updated Card class and test.

--- Card.java
+++ Card.java
@@ -1,14 +1,16 @@
-public class Card {
-    final int value;
-
-    public Card(int value) {
-        if (value < 0) {
-            throw new IllegalArgumentException("Card value must be positive");
+public record Card(int value) {
+    public Card {
+        if (value < 1) {
+            throw new IllegalArgumentException("Card value must be positive, non-zero integer");
         }
-        this.value = value;
     }
 
-    public int getValue() {
-        return value;
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Card) {
+            Card other = (Card) obj;
+            return value == other.value;
+        }
+        return false;
     }
 }
--- CardTest.java
+++ CardTest.java
@@ -1,13 +1,11 @@
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CardTest {
 
-
     @Test
     public void testGetValue() {
-        assertEquals(1, new Card(1).getValue());
+        assertEquals(1, new Card(1).value());
     }
 
     @Test
@@ -18,5 +16,21 @@ public class CardTest {
         } catch (IllegalArgumentException e) {
             assertTrue(true);
         }
+    }
+
+    @Test
+    public void testCreateZero() {
+        try {
+            new Card(0);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testCompareCardValues() {
+        assertEquals(new Card(1), new Card(1));
+        assertNotEquals(new Card(1), new Card(2));
     }
 }
