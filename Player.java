27adb6ec83e525b310181dd5e97079139f84b5c4
Timothy <tjl219@exeter.ac.uk>
2023-11-16 19:40:10 +0000

    Fixed array manipulation for array use.

--- Player.java
+++ Player.java
@@ -1,6 +1,8 @@
 // import java.util.concurrent.ArrayBlockingQueue;
 // ArrayLinkedList does not allow for removing random elements. Therefore can not be used to remove only non-preferred cards.
 
+import java.util.Arrays;
+
 public class Player implements Runnable {
     private final String name;
     private final Card[] hand;
@@ -51,25 +53,39 @@ public class Player implements Runnable {
         stopRequested = true;
     }
 
-    public Player(int playerNumber, int preferredValue, Card[] cards, CardDeck giveDeck, CardDeck takeDeck) {
+    public Player(int preferredValue, CardDeck giveDeck, CardDeck takeDeck) {
         this.hand = new Card[capacity];
         this.preferredValue = preferredValue;
 
         this.giveDeck = giveDeck;
         this.takeDeck = takeDeck;
-        
-        if (playerNumber < 1) {
-            name = "player " + playerNumber;
+
+        // Player number derived from its preferred value
+        if (preferredValue > 0) {
+            name = "player" + preferredValue;
         } else {
             throw new IllegalArgumentException("playerNumber must be > 1");
         }
+    }
 
-        System.out.printf("%s initial hand %s%n", name, hand.toString());
+    public Player(int preferredValue, CardDeck giveDeck, CardDeck takeDeck, Card[] startingHand) {
+        this(preferredValue, giveDeck, takeDeck);
+        if (startingHand.length > capacity) {
+            throw new IllegalArgumentException("startingHand must be or less than " + capacity);
+        }
+        System.arraycopy(startingHand, 0, hand, 0, startingHand.length);
+        System.out.printf("%s initial hand %s\n", name, Arrays.toString(hand));
     }
 
     // Returns the number of cards in the hand.
     public int getHandSize() {
-        return hand.length;
+        int size = 0;
+        for (Card card : hand) {
+            if (card != null) {
+                size++;
+            }
+        }
+        return size;
     }
 
     public int getPreferredValue() {
@@ -83,6 +99,43 @@ public class Player implements Runnable {
 
     public String getName() {
         return name;
+    }
+
+    // Adds card to end of queue.
+    // Throws IllegalStateException if hand is full.
+    public void pushCard(Card card) {
+        if (getHandSize() < capacity) {
+            hand[getHandSize()] = card;
+        } else {
+            throw new IllegalStateException("Hand is full");
+        }
+    }
+
+    // Removes and retrieves the first card that matches the value.
+    // Returns null if no card matches.
+    public Card popCard(int value) {
+        Card card = null;
+        for (int i=0; i<getHandSize(); i++) {
+            if (hand[i].value() == value) {
+                card = hand[i];
+                hand[i] = null;
+                i = getHandSize();
+            }
+        }
+        compressHand();
+        return card;
+    }
+
+    private void compressHand() {
+        Card[] array = new Card[capacity];
+        int pointer = 0;
+        for (Card card : hand) {
+            if (card != null) {
+                array[pointer] = card;
+                pointer++;
+            }
+        }
+        System.arraycopy(array, 0, hand, 0, capacity);
     }
 
     public boolean isWinningHand() {
