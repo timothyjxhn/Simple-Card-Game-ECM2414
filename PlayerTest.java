66e08e063748e3559d4820d72a12a40d384939d1
Timothy <tjl219@exeter.ac.uk>
2023-11-16 19:39:36 +0000

    Adapted PlayerTest for preliminary testing.

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,29 +1,42 @@
-import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-    @Test
-    public void testGetHandSize() {
-        Card[] cards = {new Card(1), new Card(2), new Card(3)};
-        Player player = new Player(1, cards);
-        assertEquals(3, player.getHandSize());
+    Card[] cardsWin1;
+    Card[] cardsWin2;
+    Card[] cardsLose3;
+    Card[] cards4NotFull;
+    Card[] cards5NotFull;
+
+    @Before
+    public void setUp() {
+        cardsWin1 = new Card[] {new Card(1), new Card(1), new Card(1), new Card(1)};
+        cardsWin2 = new Card[] {new Card(2), new Card(2), new Card(2), new Card(2)};
+        cardsLose3 = new Card[] {new Card(2), new Card(3), new Card(5), new Card(4)};
+        cards4NotFull = new Card[] {new Card(4), new Card(4), new Card(4)};
+        cards5NotFull = new Card[] {new Card(5), new Card(5)};
     }
 
     @Test
-    public void pushCard() {
-        Card[] card = {new Card(1)};
-        Player player = new Player(1, card);
-        player.pushCard(new Card(2));
+    public void testGetHandSize() {
+        Player player = new Player(1, null, null, cards5NotFull);
         assertEquals(2, player.getHandSize());
     }
 
     @Test
+    public void pushCard() {
+        Player player = new Player(1, null, null, cards4NotFull);
+        Player player2 = new Player(2, null, null);
+        player.pushCard(new Card(2));
+        player2.pushCard(new Card(4));
+        assertEquals(4, player.getHandSize());
+        assertEquals(1, player2.getHandSize());
+    }
+
+    @Test
     public void pushCardWithFullHand() {
-        Card[] cards = {new Card(1), new Card(2), new Card(3), new Card(4)};
-        Player player = new Player(1, cards);
+        Player player = new Player(1, null, null, cardsWin1);
         try {
             player.pushCard(new Card(5));
             fail("Expected IllegalStateException");
@@ -33,30 +46,23 @@ public class PlayerTest {
     }
 
     @Test
-    public void peekFirstCard() {
-    }
-
-    @Test
     public void popCard() {
+        Player player = new Player(1, null, null, cardsLose3);
+        assertEquals(3, player.popCard(3).value());
+        assertEquals(3, player.getHandSize());
     }
 
     @Test
     public void testIsWinningHand() {
-        Card[] cards = {new Card(1), new Card(1), new Card(1), new Card(1)};
-        Card[] cards2 = {new Card(2), new Card(2), new Card(2), new Card(2)};
-        Player player = new Player(1, cards);
-        Player player2 = new Player(2, cards2);
+        Player player = new Player(1, null, null, cardsWin1);
+        Player player2 = new Player(2, null, null, cardsWin2);
         assertTrue(player.isWinningHand());
         assertTrue(player2.isWinningHand());
     }
 
     @Test
     public void testIsNotWinningHand() {
-        Card[] cards = {new Card(1), new Card(2), new Card(1), new Card(2)};
-        Card[] cards3 = {new Card(3), new Card(3), new Card(3), new Card(4)};
-        Player player1 = new Player(1, cards);
-        Player player3 = new Player(3, cards3);
+        Player player1 = new Player(1, null, null, cardsLose3);
         assertFalse(player1.isWinningHand());
-        assertFalse(player3.isWinningHand());
     }
 }
