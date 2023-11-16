d8ee92fa5fe2516d34907b99f8b80e09639a2322
Timothy <tjl219@exeter.ac.uk>
2023-11-16 21:56:58 +0000

    Added tests for card deck.

---
+++ CardDeckTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class CardDeckTest {
+
+    @Test
+    public void getDeck() {
+    }
+
+    @Test
+    public void pushCard() {
+    }
+
+    @Test
+    public void popCard() {
+    }
+
+    @Test
+    public void getDeckName() {
+    }
+}
