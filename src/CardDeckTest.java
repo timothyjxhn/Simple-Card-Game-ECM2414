6de67b8c738cd79d888101d0a8d96cadfdcd2657
Timothy <tjl219@exeter.ac.uk>
2023-11-25 19:45:43 +0000

    Added file read tests for Player and CardDeck output.

--- src/CardDeckTest.java
+++ src/CardDeckTest.java
@@ -1,22 +1,61 @@
 import org.junit.*;
-
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileReader;
 
 public class CardDeckTest {
 
+    CardDeck cardDeck;
+
+    @Before
+    public void setUpEach() {
+        cardDeck = new CardDeck(1, new Card[] {new Card(1), new Card(2), new Card(3), new Card(4)});
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        CardGameTestSuite.cleanup();
+    }
+
     @Test
-    public void getDeck() {
+    public void testDeckWriteToFile() {
+        cardDeck.endDeck();
+        try (FileReader fileReader = new FileReader("deck1_output.txt")) {
+            char[] contents = new char[100];
+            fileReader.read(contents);
+            assertEquals("deck1 contents: [1, 2, 3, 4]", new String(contents).trim());
+        }
+        catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
     @Test
     public void pushCard() {
+        try {
+            cardDeck.pushCard(new Card(5));
+            assertEquals(new Card(5), cardDeck.getDeck()[4]);
+            assertEquals(5, cardDeck.getDeck().length);
+        } catch (InterruptedException e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
     @Test
     public void popCard() {
+        try {
+            Card card = cardDeck.popCard();
+            assertEquals(new Card(1), card);
+            assertEquals(3, cardDeck.getDeck().length);
+        }
+        catch (InterruptedException e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
     @Test
     public void getDeckName() {
+        assertEquals("deck1", cardDeck.getDeckName());
     }
 }
--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,48 +1,125 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.io.FileReader;
 
 public class PlayerTest {
 
-    Player player;
-    Card[] cardsWin1 = new Card[] {new Card(1), new Card(1), new Card(1), new Card(1)};
-    Card[] cardsWin2 = new Card[] {new Card(2), new Card(2), new Card(2), new Card(2)};
-    Card[] cardsLose3 = new Card[] {new Card(2), new Card(3), new Card(3), new Card(4)};
-    Card[] cardsLose4 = new Card[] {new Card(3), new Card(1), new Card(1), new Card(1)};
-    Card[] cardsLose5 = new Card[] {new Card(3), new Card(3), new Card(3), new Card(1)};
+    static Player player;
+    static Card[] cardsWin1;
+    static Card[] cardsWin2;
+    static Card[] cardsLose3;
+    static Card[] cardsLose4;
+    static Card[] cardsLose5;
 
     @Before
-    public void setUp() { }
+    public void setUpEach() {
+        cardsWin1 = new Card[] {new Card(1), new Card(1), new Card(1), new Card(1)};
+        cardsWin2 = new Card[] {new Card(2), new Card(2), new Card(2), new Card(2)};
+        cardsLose3 = new Card[] {new Card(2), new Card(3), new Card(3), new Card(4)};
+        cardsLose4 = new Card[] {new Card(3), new Card(1), new Card(1), new Card(1)};
+        cardsLose5 = new Card[] {new Card(3), new Card(3), new Card(3), new Card(1)};
+    }
+
+    // Close the file writer after each test to avoid file locking
+    @After
+    public void tearDownEach() {
+        if (player != null) player.closeWriter();
+    }
+
+    // Delete all output files after all tests have run
+    @AfterClass
+    public static void tearDown() {
+        CardGameTestSuite.cleanup();
+    }
+
+    @Test
+    public void testCorrectName() {
+        player = new Player(151, null, null, cardsWin1);
+        assertEquals("player151", player.getName());
+    }
+
+    @Test
+    public void testOutputFileSetup() {
+        player = new Player(369, null, null, cardsWin1);
+        File file = new File("player369_output.txt");
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void testPrintToFile() {
+        player = new Player(369, null, null, cardsWin1);
+        FileReader fileReader = null;
+        try {
+            Method printToFile = Player.class.getDeclaredMethod("printToFile", String.class);
+            printToFile.setAccessible(true);
+            printToFile.invoke(player, "test");
+            player.closeWriter();
+            fileReader = new FileReader("player369_output.txt");
+            char[] contents = new char[100];
+            fileReader.read(contents);
+            assertTrue(new String(contents).trim().endsWith("test"));
+        }
+        catch (Exception e) {
+            fail("Could not run test due to: " + e);
+        }
+        finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                }
+                catch (Exception ignored) { }
+            }
+        }
+    }
+
+    @Test
+    public void testStartWithMoreThanFourCards() {
+        try {
+            player = new Player(1, null, null, new Card[] {new Card(1), new Card(1), new Card(1), new Card(1),
+                    new Card(1), new Card(1), new Card(1), new Card(1)});
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
 
     @Test
     public void testSwapCardKeepsPreferred() {
-        Card card;
-
         player = new Player(1, null, null, cardsLose3);
-        card = player.swapCard(new Card(1));
+        Card card = player.swapCard(new Card(1));
         assertNotEquals(3, card.value());
+    }
 
-        player = new Player(1, null, null, cardsLose4);
-        card = player.swapCard(new Card(1));
-        assertNotEquals(1, card.value());
-
+    @Test
+    public void testSwapCardKeepsPreferred2() {
         player = new Player(1, null, null, cardsLose5);
-        card = player.swapCard(new Card(1));
+        Card card = player.swapCard(new Card(1));
         assertNotEquals(3, card.value());
     }
 
     @Test
     public void testIsWinningHand() {
-        Player player = new Player(1, null, null, cardsWin1);
-        Player player2 = new Player(2, null, null, cardsWin2);
+        player = new Player(1, null, null, cardsWin1);
         assertTrue(player.isWinningHand());
-        assertTrue(player2.isWinningHand());
+    }
+
+    @Test
+    public void testIsWinningHand2() {
+        player = new Player(2, null, null, cardsWin2);
+        assertTrue(player.isWinningHand());
     }
 
     @Test
     public void testIsNotWinningHand() {
-        Player player1 = new Player(1, null, null, cardsLose3);
-        Player player2 = new Player(2, null, null, cardsLose5);
-        assertFalse(player1.isWinningHand());
-        assertFalse(player2.isWinningHand());
+        player = new Player(1, null, null, cardsLose3);
+        assertFalse(player.isWinningHand());
+    }
+
+    @Test
+    public void testIsNotWinningHand2() {
+        player = new Player(2, null, null, cardsLose5);
+        assertFalse(player.isWinningHand());
     }
 }
